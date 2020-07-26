c4ae1776cd3e311d15cc22c1e56999af12df8de8
akashgarg3a <akashgarg3a@gmail.com>
2020-07-27 02:39:43 +0530

    remove

--- scrapping/ropphantomcoc.js
+++
@@ -1,6 +0,0 @@
-var webpage = require('webpage').create()
-
-webpage.open('https://www.espncricinfo.com/series/19322/commentary/1187684/', function() {
-    webpage.render('scotch.jpeg');
-    phantom.exit();
-});
